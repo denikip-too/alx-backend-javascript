94ee8b47f4f0488676c682b76914a321b11f3f36
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-15 15:01:39 -0700

    JS

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// modifying variables inside the function so that then are not overwritten
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
