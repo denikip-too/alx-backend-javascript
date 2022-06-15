1e4fe64139fda77e48cbedc442f8c56e7a7f8b84
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-15 14:58:36 -0700

    JS

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+// using for...of operator
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx of array) {
+    let value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+// using ES6’s arrow syntax of the function add
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = add(newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
