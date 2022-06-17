a4ac559b74449c4bcc8133e9efb7d52d46b3c934
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-17 14:13:48 -0700

    Using spread syntax

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,5 @@
+// function that concatenate 2 arrays and each character of a string
+
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
