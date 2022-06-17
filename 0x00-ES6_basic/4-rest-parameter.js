b3108bb94dcbf0b0d30ea4e85a9ab2957bdf0818
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-17 14:03:50 -0700

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 // return the number of arguments passed to it using the rest parameter syntax
 
 export default function returnHowManyArguments(...args) {
-  console.log(args.length);
+  return args.length;
 }
