bb1cc43c0f728a4a0d862d4cf42ab3e153b96ee3
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-21 16:59:22 -0700

    returns an array of ids from a list of object

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+// function getListStudentIds that returns an array of ids from list of object
+
+default export function getListStudentIds (id) {
+  const arrs = new Map();
+  return [arrs.get(id)]
+}
