bb1ea3cb55c2c2fe37ad9a790d2ff2182747998a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-21 17:07:15 -0700

    returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+// returns the sum of all the student ids
+
+import getListStudents from "./0-get_list_students.js";
+
+
+let sum = arr.reduce(function getStudentIdsSum (list) {
+  return list.id;
+}
