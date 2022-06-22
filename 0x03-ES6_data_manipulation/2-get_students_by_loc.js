5723d05a345a5b3e56d1f6f62c197ba9293dcd13
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-21 17:04:30 -0700

    returns an array of objects who are located in a specific city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,9 @@
+// returns an array of objects who are located in a specific city
+
+import getListStudents from "./0-get_list_students.js";
+
+
+export default function getStudentsByLocation ([], city) {
+  const list = arr.filter(city => city.location);
+  return list;
+}
