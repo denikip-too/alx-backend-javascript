1ecf72ec814a6aff596fec69a977aef279a89687
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-27 16:04:20 -0700

    Return promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,8 +4,7 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({ status: 200, body: 'Success' });
-    }
-    else {
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
