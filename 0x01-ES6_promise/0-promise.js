0e5b6d4f2bcca40d2e07b3d38119c29e228f1795
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-27 14:50:25 -0700

    Return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 // Return a Promise using this prototype function getResponseFromAPI()
 
 export function getResponseFromAPI() {
-  const promise = new Promise((success, reject) => {
-    [sucess, reject]
+  const promise = new Promise((resolve, reject) => {
+    [resolve, reject]
   });
   return promise;
 }
