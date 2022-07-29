5786a4b5d26de683f672f6299b7bade982357da6
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-29 16:05:13 -0700

    Implement a methos

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,7 +5,7 @@ export default class Currency {
   }
   set name(value) {
     if (typeof value !== 'string') {
-      throw new Error('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
