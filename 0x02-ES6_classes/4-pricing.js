c488b236f8f974f2b83f8f7cc8f89a7de3e441e8
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-30 01:57:38 -0700

    JS

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(value) {
+    if (typeof value  !== 'number') {
+      throw new TypeError('Amount must be an integer');
+    }
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency} (${Currency.code})`
+  }
+}
