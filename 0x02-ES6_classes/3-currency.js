17220b9cde6e29b2b367299474ef378918c63b29
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-07-30 00:55:28 -0700

    Implements a method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,21 +3,25 @@ export default class Currency {
     this.name = name;
     this.code = code;
   }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
+
   get name() {
     return this._name;
   }
+
   set code(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
+
   get code() {
     return this._code;
   }
