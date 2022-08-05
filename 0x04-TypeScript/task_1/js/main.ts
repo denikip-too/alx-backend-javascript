bae6c637ae2d068f2ce6b1524cf985e02b594e85
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-05 15:49:38 -0700

    push

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,13 @@ interface Teacher {
   location: string = "default",
   contract: boolean
 }
+interface Directors extends Teacher {
+  numberOfReports: number
+}
+interface printTeacherFunction {
+  firstName: string,
+  lastName: string
+}
+function printTeacher(firstName: string, lastName: string) : printTeacherFunction {
+  console.log(`Array.from(${firstName})[0]"."${lastName}`);
+}
