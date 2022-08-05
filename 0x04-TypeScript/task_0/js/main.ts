351418ce67c08661885497fc0ae28460dbdbd49f
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-05 14:36:59 -0700

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,18 @@
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+};
+const student: Student = {
+  firstName: "Denis",
+  lastName: "Kiprotich",
+  age: 21,
+  location: "Kenya, Nairobi"
+};
+const student: Student = {
+  firstName: "John",
+  lastName: "Wahome",
+  age: 22,
+  lacation: "Kenya, Nairobi"
+};
