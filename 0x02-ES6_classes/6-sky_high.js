17bbcc96e6666332d20a4607d124a1bd566c9d2c
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-08 23:43:34 +0000

    JS

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    this.sqft = sqft;
+    this.floors = floors;
+  }
+  
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Evacuate slowly the NUMBER_OF_FLOORS floors');
+  }
+}
