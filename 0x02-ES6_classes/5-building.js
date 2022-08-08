2d4e93055827fd49e8566016e4075cc8a92820df
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-08 23:30:28 +0000

    JS

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this.sqft = sqft;
   }
 
