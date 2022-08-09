489b225fa845cfbaac5295aad293327b51d06389
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-09 01:19:33 +0000

    JS

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 // return an array of 3 ClassRoom objects with the sizes 19, 20, and 34
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms () {
+export default function initializeRooms() {
   const array = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
   return array;
 }
