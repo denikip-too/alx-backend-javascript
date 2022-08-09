f12af88c68afd2687382e6e4118ba14e7bacbb35
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-09 22:40:46 +0000

    JS

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,5 @@
 export default function signUpUser(firstName, lastName) {
   return new Promise((resolve) => {
-    if (firstName === true && lastName === true) {
-      resolve(`{firstName: ${firstName}, lastName: ${lastName}}`);
-    }
+    resolve(`{firstName: ${firstName}, lastName: ${lastName}}`);
   });
 }
