8dc1171cd8bbf3cc75dff6fcb23efc119a5070bc
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-09 22:01:27 +0000

    Handle multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,14 @@
 // import uploadPhoto and createUser from utils.js
 // and log body firstName lastName to the console
 
-import uploadPhoto, createUser from "./utils";
+import uploadPhoto from "./utils";
+import createUser from "./utils";
 
 export default function handleProfileSignup() {
-  promise.all(${body} ${firstName} ${lastName}).then((values) => {
+  promise.all(`${body} ${firstName} ${lastName}`).then((values) => {
     console.log(values);
+  })
+  .catch((err) => {
+    console.log("Signup system offline");
   });
 }
