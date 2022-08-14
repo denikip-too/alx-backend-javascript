93ed5cc3389c95eac56c111eff8623baa6656278
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-14 11:14:16 +0000

    JS

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 // function named getListStudents that returns an array of objects
 
-export default function getListStudents () {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Colombia' },
-    { id: 3, firsName: 'Serena', location: 'San Francisco' }
+    { id: 3, firsName: 'Serena', location: 'San Francisco' },
   ];
 }
