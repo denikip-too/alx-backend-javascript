100a8f08348e438fa3fd38fd7697561a8655c5a5
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-22 23:45:00 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,9 +1,16 @@
-const calculateNumber = require('./0-calcul.js');
-const calculateNumberTest = new calculateNumber();
+//const calculateNumber = require('./0-calcul.js');
+//const calculateNumberTest = new calculateNumber();
 
-test('testing addition', () => {
-  expect(calculateNumberTest.calculateNumber('1, 3')).toBe(4);
-  expect(calculateNumberTest.calculateNumber('1, 3.7')).toBe(5);
-  expect(calculateNumberTest.calculateNumber('1.2, 3.7')).toBe(5);
-  expect(calculateNumberTestcalculateNumber('1.5, 3.7')).toBe(6);
-});
+//test('testing addition', () => {
+//  expect(calculateNumberTest.calculateNumber('1, 3')).toBe(4);
+  //expect(calculateNumberTest.calculateNumber('1, 3.7')).toBe(5);
+ // expect(calculateNumberTest.calculateNumber('1.2, 3.7')).toBe(5);
+ // expect(calculateNumberTestcalculateNumber('1.5, 3.7')).toBe(6);
+//});
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+assert.equal(calculateNumber(1, 3), 4)
+assert.equal(calculateNumber(1, 3.7), 5)
+assert.equal(calculateNumber(1.2, 3.7), 5)
+assert.equal(calculateNumber(1.5, 3.7), 6)
