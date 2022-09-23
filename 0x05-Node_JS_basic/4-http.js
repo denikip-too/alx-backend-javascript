4bbd2670abdea1f0d7496e9a6a40ac81f2508352
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-23 21:07:51 +0000

    fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const host = 'localhost';
+const host = '127.0.0.1';
 const port = 1245;
 const app = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/csv");
