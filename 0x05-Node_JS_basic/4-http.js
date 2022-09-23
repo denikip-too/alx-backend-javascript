ae3338a17488bd2f25af5c52822b55977bb1311a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-23 21:12:47 +0000

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const http = require('http');
 
-const host = '127.0.0.1';
+const host = 'localhost';
 const port = 1245;
 const app = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/csv");
+  res.setHeader('Content-Type', 'text/csv');
   res.writeHead(200);
   res.end('Hello Holberton School!');
 });
