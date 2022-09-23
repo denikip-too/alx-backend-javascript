4f1e0158f61f132323bfbeea09252136de831d31
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-23 19:46:08 +0000

    push

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,10 +1,40 @@
-const fs = require("fs");
+const fs = require('fs');
+
 function countStudents(path) {
-  fs.readFileSync("./database.csv", (err, data) => {
-    if (!err) {
-      console.log("Number of students: " + data.length)
-    } else {
-      console.error("Cannot load the database");
+  let content;
+
+  try {
+    content = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  content = content.toString().split('\n');
+
+  let students = content.filter((item) => item);
+
+  students = students.map((item) => item.split(','));
+
+  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
     }
-  });
+  }
+
+  delete fields.field;
+
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}.
+        List: ${fields[key].join(', ')}`,
+    );
+  }
 }
+
+module.exports = countStudents;
