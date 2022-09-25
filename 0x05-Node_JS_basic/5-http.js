cd2adcba4ec788b501d82b2b97fb9c91d64f0bbd
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-25 09:05:27 +0000

    JS

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,8 +3,9 @@ const countStudents = require('./3-read_file_async');
 
 const host = 'localhost';
 const port = 1245;
+const db = process.argv[0]
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", process.argv[2]);
   switch(req.url) {
     case '/':
@@ -13,11 +14,16 @@ const app = http.createServer((req, res) => {
       break
     case '/students':
       res.writeHead(200);
-      
-      res.write('This is the list of our students');
-      res.end(countStudents)
+      res.write('This is the list of our students\n');
+      try {
+        const students = await countStudents(db);
+        res.end(`${students.join('\n')}`);
+      } catch (error) {
+        res.end(error.message);
+      }
     default:
       res.writeHead(404);
+      res.end()
   }
 });
 app.listen(port, host, () => {
